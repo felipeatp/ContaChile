ce260d36be5b548de2630646527ec8f97a66b042
felipeatp <felipe.troncosopt@gmail.com>
2026-05-13 00:46:49 -0400

    feat(dte): wire type 33 plugin into registry

---
+++ packages/dte/src/index.ts
@@ -0,0 +1,17 @@
+import { registerType } from './registry'
+import { generateFactura33 } from './generators/factura-33'
+import { validateBusinessRules } from './validators/business'
+import { DocumentData } from './types'
+
+registerType({
+  code: 33,
+  name: 'Factura Electrónica',
+  validate: (data: unknown) => validateBusinessRules(data as DocumentData),
+  generateXML: generateFactura33,
+  generatePDF: () => Buffer.from('pdf-stub'),
+  requiredFields: ['receiver', 'items'],
+})
+
+export { runPipeline } from './pipeline'
+export { registerType, getTypePlugin } from './registry'
+export type { DocumentData, DocumentTypePlugin, PipelineResult } from './types'
